552202c664deac5a6649f7b0c47ee4acc2ce104a
Brandon <jason-deen@outlook.com>
2025-05-31 11:03:00 +0200

    ✨ Get items from stripe API and displays them on home and product page

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference path="../.astro/types.d.ts" />
+interface ImportMetaEnv {
+	readonly PUBLIC_STRIPE_KEY: string;
+	readonly SECRET_STRIPE_KEY: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
